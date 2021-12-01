26f5e645cdad6ce87d72b2f045544fdfb65d762f
MrSpoony <kimi.loeffel@hispeed.ch>
2021-12-01 22:39:23 +0100

    c++ template

---
+++ nvim/templates/cpp.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FOR(i, j, k, in) for (long long int i=j ; i<k ; i+=in)
+#define RFOR(i, j, k, in) for (long long int i=j ; i>=k ; i-=in)
+#define REP(i, j) FOR(i, 0, j, 1)
+#define RREP(i, j) RFOR(i, j, 0, 1)
+#define FOREACH(i, j) for (auto i : j) 
+#define MP make_pair
+#define PB push_back
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#define read(type) readInt<type>()
+const double pi=acos(-1.0);
+
+typedef long long int lint;
+typedef priority_queue<lint> PQI;
+typedef priority_queue<lint, vector<lint>, greater<lint>> RPQI;
+typedef pair<lint, lint> PII;
+typedef vector<lint> VI;
+typedef vector<PII> VII;
+typedef vector<string> VS;
+typedef vector<VI> VVI;
+typedef vector<VII> VVVI;
+
+
+
+int main() {
+	ios_base::sync_with_stdio(false); // Desync c++ and c input/output
+	cin.tie(0); // disable automatic input flush respectively making output then when it's couted
+
+
+	return 0;
+}
