b0295e25f55a577ba7ece20643e49f4f167b0a63
MrSpoony <kimi.loeffel@hispeed.ch>
2022-02-10 22:51:31 +0100

    new cpp template with better infinities removed never used stuff and better pi

--- nvim/templates/cpp.cpp
+++ nvim/templates/cpp.cpp
@@ -9,27 +9,28 @@ using namespace std;
 #define FOREACH(i, j) for (auto i : j) 
 #define MP make_pair
 #define PB push_back
-#define RB pop_pack
-#define INF (int)1e9
-#define EPS 1e-9
+#define INF 2147483647
+#define INFINITE 9223372036854775807
 #define PI 3.1415926535897932384626433832795
-#define read(type) readInt<type>()
+#define endl "\n";
 
 typedef long long int lint;
 typedef priority_queue<lint> PQI;
 typedef priority_queue<lint, vector<lint>, greater<lint>> RPQI;
 typedef pair<lint, lint> PII;
 typedef vector<lint> VI;
+typedef vector<bool> VB;
 typedef vector<PII> VII;
-typedef vector<VI> VVI;
 typedef vector<string> VS;
+typedef vector<VI> VVI;
 
 
 
 int main() {
     ios_base::sync_with_stdio(false); // Desync c++ and c input/output
-    cin.tie(0); // disable automatic input flush respectively making output then when it's couted
+    cin.tie(0); // Disable automatic input flush respectively making output then when it's couted
 
+    
 
     return 0;
 }
