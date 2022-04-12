831605641ef622c3d435e205cc76bd8ae39350f0
MrSpoony <kimi.loeffel@hispeed.ch>
2022-04-12 20:48:55 +0200

    Stow script

---
+++ stow.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+stow alacritty
+stow bin
+stow bspwm
+stow gdb
+stow important
+stow lazygit
+stow nvim
+stow picom
+stow polybar
+stow rofi
+stow spotifyd
+stow tmux
+stow wallpapers
+stow wpg
+stow X
