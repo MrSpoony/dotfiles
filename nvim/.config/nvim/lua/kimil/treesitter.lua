a670ab8d3f6eaf96e5ac4b4c123b44e39b82bc30
MrSpoony <kimi.loeffel@hispeed.ch>
2022-04-14 01:20:13 +0200

    New treesitter textobjects and -subjects

--- nvim/.config/nvim/lua/kimil/treesitter.lua
+++ nvim/.config/nvim/lua/kimil/treesitter.lua
@@ -1,5 +1,5 @@
-require'nvim-treesitter.configs'.setup {
-    ensure_installed = {"bash", "c", "c_sharp", "cmake", "cpp", "css", "dart", "dot", "fish", "go", "gomod", "haskell", "html", "http", "java", "javascript", "latex", "llvm", "lua", "make", "markdown", "nix", "python", "rust", "ruby", "json", "toml", "typescript", "vim", "yaml", "vue"},
+require 'nvim-treesitter.configs'.setup {
+    ensure_installed = { "bash", "c", "c_sharp", "cmake", "cpp", "css", "dart", "dot", "fish", "go", "gomod", "haskell", "html", "http", "java", "javascript", "latex", "llvm", "lua", "make", "markdown", "nix", "python", "rust", "ruby", "json", "toml", "typescript", "vim", "yaml", "vue" },
     sync_install = true,
     incremental_selection = {
         enable = true,
@@ -8,7 +8,7 @@ require'nvim-treesitter.configs'.setup {
             node_incremental = "grn",
             scope_incremental = "grc",
             node_decremental = "grm",
-            },
+        },
     },
     indent = {
         enable = true,
@@ -34,6 +34,68 @@ require'nvim-treesitter.configs'.setup {
     query_linter = {
         enable = true,
         use_virtual_text = true,
-        lint_events = {"BufWrite", "CursorHold"},
+        lint_events = { "BufWrite", "CursorHold" },
+    },
+    textsubjects = {
+        enable = true,
+        prev_selection = ';', -- (Optional) keymap to select the previous selection
+        keymaps = {
+            ['.'] = 'textsubjects-smart',
+            [','] = 'textsubjects-container-outer',
+            ['i,'] = 'textsubjects-container-inner',
+        },
+    },
+    textobjects = {
+        select = {
+            enable = true,
+
+            -- Automatically jump forward to textobj, similar to targets.vim
+            lookahead = true,
+
+            keymaps = {
+                -- You can use the capture groups defined in textobjects.scm
+                ["af"] = "@function.outer",
+                ["if"] = "@function.inner",
+                ["ac"] = "@class.outer",
+                ["ic"] = "@class.inner",
+            },
+        },
+        swap = {
+            enable = true,
+            swap_next = {
+                ["<leader>a"] = "@parameter.inner",
+            },
+            swap_previous = {
+                ["<leader>A"] = "@parameter.inner",
+            },
+        },
+        move = {
+            enable = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_next_start = {
+                ["]m"] = "@function.outer",
+                ["]]"] = "@class.outer",
+            },
+            goto_next_end = {
+                ["]M"] = "@function.outer",
+                ["]["] = "@class.outer",
+            },
+            goto_previous_start = {
+                ["[m"] = "@function.outer",
+                ["[["] = "@class.outer",
+            },
+            goto_previous_end = {
+                ["[M"] = "@function.outer",
+                ["[]"] = "@class.outer",
+            },
+        },
+        lsp_interop = {
+            enable = true,
+            border = 'none',
+            peek_definition_code = {
+                ["<leader>df"] = "@function.outer",
+                ["<leader>dF"] = "@class.outer",
+            },
+        },
     },
 }
