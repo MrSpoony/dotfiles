47c8c2d0be1600898d736d4358e9f8e37421d00c
MrSpoony <kimi.loeffel@hispeed.ch>
2022-04-16 01:14:37 +0200

    All set options in lua now and some remappings

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,118 +1,113 @@
 require('plugins')
 
-vim.cmd [[
+local o = vim.opt
 
+o.clipboard = { 'unnamed', 'unnamedplus' }
+
+o.spelllang = "en_US"
+o.spell = false
+o.encoding = 'utf-8'
+
+o.shiftwidth = 4
+o.tabstop = 4
+o.expandtab = true
+o.autoindent = true
+o.number = true
+o.relativenumber = true
+o.signcolumn = 'yes'
+
+o.textwidth = 0
+o.colorcolumn ='80'
+o.wrap = true
+o.linebreak = true
+
+o.cmdheight=2
+o.modifiable = true
+o.wildmenu = true
+o.wildmode= { 'longest:full','full' }
+
+o.showmode = false
+o.ruler = true
+o.showcmd = true
+o.laststatus = 2
+
+o.cmdheight = 2
+o.incsearch = true
+o.hlsearch = true
+o.ignorecase = false
+o.path:append("**")
+
+o.hidden = true
+o.showmatch = true
+o.autoread = true
+o.swapfile = false
+o.updatetime = 100
+o.backup = false
+o.history = 10000
+o.undofile = true
+
+o.scrolloff = 4
+o.mouse = "nvc"
+
+o.visualbell = true
+
+o.splitright = true
+o.splitbelow = true
+
+o.modifiable = true
+
+o.background = "dark"
+o.termguicolors = true
+
+
+vim.g.mapleader = " "
+
+require("kimil")
+
+Command("WQ", "wq")
+Command("Wq", "wq")
+Command("W", "w")
+Command("Q", "q")
+
+Inoremap(":w<CR>", "<Esc>:w<CR>")
+Inoremap(":wq<CR>", "<Esc>:wq<CR>")
+Inoremap(":q!<CR>", "<Esc>:q!<CR>")
+
+Nnoremap("j", "gj")
+Nnoremap("k", "gk")
+Nnoremap("gj", "j")
+Nnoremap("gk", "k")
+
+Vnoremap("j", "gj")
+Vnoremap("k", "gk")
+Vnoremap("gj", "j")
+Vnoremap("gk", "k")
+
+Nnoremap("<Up>", "<Nop>")
+Nnoremap("<Down>", "<Nop>")
+Nnoremap("<Right>", "<Nop>")
+Nnoremap("<Left>", "<Nop>")
+
+Vnoremap("<Up>", "<Nop>")
+Vnoremap("<Down>", "<Nop>")
+Vnoremap("<Right>", "<Nop>")
+Vnoremap("<Left>", "<Nop>")
+
+vim.cmd([[
 filetype plugin on
 syntax on
+]])
 
-set textwidth=0                   " Don't insert EOLs at linebreak
-set colorcolumn=80                " Column at line 80 so I know where to make a carriage return
-set wrap                          " Wrap to long lines around
-set linebreak                     " Wrap lines at the breakat parameter (which is currently not set)
-
-set noshowmode                    " Don't show current mode (INSERT, NORMAL, etc.)"
-set ruler                         " Always show cursor position
-set showcmd                       " Display incomplete commands on lower right
-set laststatus=2                  " Always show statusline
-
-set cmdheight=2                   " Use 2 lines for the commandline
-set filetype=on                   " Set the standard filetype to on
-set modifiable                    " Buffer can be edited
-set wildmenu                      " Enable wildmenu
-set wildmode=longest:full,full    " How wild mode should behave
-
-set incsearch                     " Incremental search
-set hlsearch                      " Highlight search results
-set noignorecase                  " Don't ignore case when searching
-set path+=**                      " Search everywhere
-
-set hidden                        " Edit another buffer whine another one is unsaved IMPORTANT ONE!!
-
-set ofu=syntaxcomplete#Complete   " Autocompletion so that menu will always appear
-set showmatch                     " highlight matching braces
-
-set autoread                      " Automatically read a file that has changed on the disk
-set noswapfile                    " Don't use a swapfile
-set updatetime=100                " Doesn't matter since I disabled writing to the swapfile
-set nobackup                      " Don't create annoying backup files
-set history=10000                 " Store last 1000 commands in history
-set undofile                      " Save undo stuff for undotree
-
-set scrolloff=3                   " Keep 3 empty lines at the borders when moving with j and k
-set mouse=nvc                     " Use mouse in Normal, Visual and Command mode
-set visualbell                    " No annoying bell
-
-set splitright                    " Put the new window to the right
-set splitbelow                    " Put the new window at the bottom
-
-set modifiable                    " Buffer can be edited
+vim.cmd([[
+autocmd ColorScheme * highlight Normal ctermbg=none guibg=none
+]])
 
 
-
-" Set mapleader to space
-let mapleader =" "
-
-
-" Load lua configurations
-lua require("kimil")
-
-" Capital case does the same
-command WQ wq
-command Wq wq
-command W w
-command Q q
-inoremap :w<CR> <Esc>:w<CR>
-inoremap :q<CR> <Esc>:q<CR>
-inoremap :q!<CR> <Esc>:q!<CR>
-
-
-" Movement
-nnoremap j gj
-nnoremap gj j
-nnoremap k gk
-nnoremap gk k
-
-vnoremap j gj
-vnoremap gj j
-vnoremap k gk
-vnoremap gk k
-
-noremap <Up> <Nop>
-noremap <Down> <Nop>
-noremap <Left> <Nop>
-noremap <Right> <Nop>
-
-" If I want to use my aliases in vimterminal but also prints the fastfetch
-" set shellcmdflag=-ic
-
-" Indenting 
-" set foldmethod=indent
-" set foldnestmax=1
-
-" Themes stuff
-" Transparent background
-au ColorScheme * hi Normal ctermbg=none guibg=none
-au ColorScheme myspecialcolors hi Normal ctermbg=red guibg=red
-set background=dark
-
-" Comments
-highlight Comment cterm=italic
-
-" WAL
-" colorscheme wal
-
-
-" ONEDARK
-if (empty($TMUX))
-  if (has("nvim"))
-    let $NVIM_TUI_ENABLE_TRUE_COLOR=1
-  endif
-  if (has("termguicolors"))
-    set termguicolors
-  endif
-endif
+vim.cmd([[
 colorscheme onedark
-
+autocmd ColorScheme * highlight Comment cterm=italic
+]])
+vim.cmd([[
 let g:netrw_liststyle = 3
 
 " Shortcuts
@@ -157,14 +152,14 @@ vnoremap : ;
 map <leader>z :Files<CR>
 map <leader>b :Ex<CR>
 
-command Pi PlugInstall
-command Pu PlugUpdate
+command! Pi PlugInstall
+command! Pu PlugUpdate
 
 " Trims all lines at end of files on save
-function TrimEndLines()
+function! TrimEndLines()
     let save_cursor = getpos(".")
     silent! %s#\($\n\s*\)\+\%$##
     call setpos('.', save_cursor)
 endfunction
 autocmd BufWritePre * call TrimEndLines()
-]]
+]])
