9f6ef417bcbcac99921ee845325230a5625d0216
MrSpoony <kimi.loeffel@hispeed.ch>
2022-04-17 17:54:49 +0200

    Some telescope stuff

--- nvim/.config/nvim/lua/kimil/telescope.lua
+++ nvim/.config/nvim/lua/kimil/telescope.lua
@@ -3,34 +3,34 @@ local builtin = require('telescope.builtin')
 
 ts.setup {
     defaults = {
-        -- Default configuration for telescope goes here:
-        -- config_key = value,
+        sorting_strategy = "ascending",
+        layout_config = {
+            prompt_position = "top",
+        },
         mappings = {
-            i = {
-                ["<C-h>"] = "which_key"
-            }
         }
     },
     pickers = {
         -- Default configuration for builtin pickers goes here:
         -- picker_name = {
         --   picker_config_key = value,
-        --   ...
         -- }
-        -- Now the picker_config_key will be applied every time you call this
-        -- builtin picker
     },
     extensions = {
-
         fzf = {
             fuzzy = true, -- false will only do exact matching
             override_generic_sorter = true, -- override the generic sorter
             override_file_sorter = true, -- override the file sorter
             case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-        }
+        },
     }
 }
+
+
 ts.load_extension('fzf')
+ts.load_extension('zoxide')
+
+
 
 Nnoremap("<leader>ff", LuaFn(builtin.find_files))
 Nnoremap("<C-p>",      LuaFn(builtin.git_files))
