e1dbecb17310f8d4043261884d86ed88180742ee
MrSpoony <kimiloeffel@gmail.com>
2022-07-08 09:30:38 +0200

    old clear script

--- bin/bin/clearcommand.sh
+++ bin/bin/clearcommand.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
 printf '\033\143'
+# clear
+
+NUMBER_OF_COMMANDS=4
+RNDNR=$((RANDOM%NUMBER_OF_COMMANDS))
+
+case $RNDNR in
+
+    0)
+        colorscript random;
+        ;;
+    1)
+        fastfetch;
+        ;;
+    2)
+        rxfetch;
+        ;;
+    3)
+        pfetch;
+        ;;
+esac
