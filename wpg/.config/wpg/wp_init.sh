5ca85ebcde2087ddfcac0a568de5faa04375a565
MrSpoony <kimi.loeffel@hispeed.ch>
2022-07-24 19:04:01 +0200

    My newest changes and merged commits from BBC

--- wpg/.config/wpg/wp_init.sh
+++ wpg/.config/wpg/wp_init.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-wpg -rs '/home/kimil/pictures/wallpapers/0171.jpg' '/home/kimil/pictures/wallpapers/0171.jpg'
+wpg -rs '/home/kimil/pictures/wallpapers' '/home/kimil/pictures/wallpapers'
