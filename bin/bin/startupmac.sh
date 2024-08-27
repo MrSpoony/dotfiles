4448a5da5ad41d90ba8ce4aea3ae4670bfb69345
MrSpoony <kimi.loeffel@hispeed.ch>
2024-08-27 10:53:44 +0200

    new scripts from mac

--- bin/bin/startupmac.sh
+++ bin/bin/startupmac.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x70000002A}]}'
+hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x70000002A},{"HIDKeyboardModifierMappingSrc":0x700000035,"HIDKeyboardModifierMappingDst":0x700000064},{"HIDKeyboardModifierMappingSrc":0x700000064,"HIDKeyboardModifierMappingDst":0x700000035},{"HIDKeyboardModifierMappingSrc":0x700000032,"HIDKeyboardModifierMappingDst":0x700000036}]}'
+defaults write -g ApplePressAndHoldEnabled 1
+sudo -u u80860794 gitlab-runner stop
